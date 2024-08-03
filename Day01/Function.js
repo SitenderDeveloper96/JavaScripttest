e544d19a09ff49912d1b8f3591be9047258cfb1a
Developer Sitender <123143157+SitenderDeveloper96@users.noreply.github.com>
2024-08-03 16:26:55 +0000

    container config done

---
+++ Day01/Function.js
@@ -0,0 +1,23 @@
+// function Saymy_Name (){
+//     console.log("My name is Saymy");
+    
+// }
+
+// Saymy_Name()
+
+function mathematics(Num1, Num2) {
+    return Num1 + Num2;
+}
+
+let result = mathematics(5, 3);
+
+console.log(result);
+
+function user(username) {
+    if(!username){
+        console.log("Enter the User name please");
+        retune
+    }
+   return username;
+}
+console.log ("user name is >>"+ user () );
